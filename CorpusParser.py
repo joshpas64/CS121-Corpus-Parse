bba12720c2eecf19603bc1564f0cfaa67f7e9702
Joshua Pascascio <jpascasc@uci.edu>
2017-05-22 23:14:57 -0700

    Adding Corpus Parser that can handle multi-word queries
    Can also save to database so the object can be retrieved without having to parse through whole Corpus again.
    Finished a full run through of 'Irvine' will do rest in coming days.
    By the end of a run through usually use .save method to save the updated document object into my MongoDB database

--- CorpusParser.py
+++ CorpusParser.py
@@ -2,19 +2,31 @@ from bs4 import BeautifulSoup
 import IndexWeights
 import os
 import re
-import nltk
-import json
-from pprint import pprint
+from pprint import pprint ##For debugging
 from lxml import etree, html
 from bson import ObjectId
 from pymongo import MongoClient
 from html.parser import HTMLParser
+
+###BONUS POINT
+## Remove stop words in tokenizing steps use NLTK
+## Using stemming or collapsing similar words into one word
+##  for similar word queries
+## Add GUI window
+
+
+##NLTK Library check
+##how to get out of codec errors
+##use open("filename","r",encoding="ASCII" or encoding="UTF-8")
+##
+
 ## The only tags to search for in the html files noted in the canvas document
 ## Style will be inverted index
 KEY_TAGS = ["h1","h2","h3","b","strong"]
 TAG_EXP = r'<title>|<h1>|<h2>|<h3>|<b>|<strong>|<p>|</p>|&amp|&lt|&gt'
 ## The json file gives a key, map of url -> corresponding filepath
 EXCLUDED_FILENAMES = ["bookkeeping.json","bookkeeping.tsv"]
+DOCUMENT_TOTAL = 37497
 ## Inverted Index Construction Sort Terms in documents alphabetically
 ## Key-Value: Term -> List of DocIds in ascending order ([1,4,6,12,20])
 ## Goal of this program script turn key terms into
@@ -25,21 +37,16 @@ EXCLUDED_FILENAMES = ["bookkeeping.json","bookkeeping.tsv"]
 ##      "h3":55,"b":88,"strong":92,priority_sum:523}
 HOST = "localhost"
 PORT = 27017
-client = MongoClient(HOST,PORT)
-use_db = client["HW3_Corpus"]
-mainCollection = use_db["HW3_Corpus"]
-docs = mainCollection.find({})
-sample_obj = mainCollection.find_one({"query":"Mondingo"})
+DOC_TOTAL = 37497
+tsv_file = open("WEBPAGES_CLEAN/bookkeeping.tsv","r",encoding="utf-8")
+tsv_content = tsv_file.readlines()
+tsv_file.close()
+TSV_PAIRS = []
+for line in tsv_content:
+    pair = line.split()
+    TSV_PAIRS.append(pair)
 #mainCollection.save(sample_obj)
 #pprint.pprint(mainCollection.find_one({"query":"Mondingo"}))
-json_file = open("WEBPAGES_CLEAN/bookkeeping.json","r")
-json_content = json_file.read()
-json_file.close()
-json_obj = json.loads(json_content)
-JSON_PAIRS = list(json_obj.items())
-## The individual files do not contain any information 
-print(JSON_PAIRS[0])
-print(len(json_obj))
 def check_if_clean(tagList):
     count = -1
     for i in tagList:
@@ -78,30 +85,40 @@ def makeDocument(word,dbTable):
         baseDocument["rank"] = -1
         dbTable.insert(baseDocument)
         return baseDocument
+def cleanUpTags(tag_exp,line):
+    tagFree = re.split(tag_exp,line)
+    tagLessString = "".join(tagFree)
+    parsed_list = re.split(r'\W+',tagLessString)
+    return parsed_list
 class DbEntry:
     def __init__(self,dbHost,dbPort,dbName,tableName,searchQuery):
         self.mainCollection = makeMongoCollection(dbHost,dbPort,dbName,tableName)
-        self.searchPhrase = searchQuery
-        self.queryDocument = makeDocument(self.searchPhrase,self.mainCollection)
+        self.searchPhrase = searchQuery.lower().strip()
+        self.searchToken = IndexWeights.makeSearchToken(self.searchPhrase)
+        self.mweToken = IndexWeights.makeIntoMWEToken(self.searchPhrase)
+        self.queryDocument = makeDocument(self.searchToken,self.mainCollection)
     def checkTitle(self,content,soupObj):
         titleString = soupObj.find('title')
         if titleString != None:
-            return IndexWeights.getWordCount(self.searchPhrase,titleString.string.split())
+            if titleString.string == None:
+                return 0
+            return IndexWeights.getWordCount(self.searchToken,self.mweToken,titleString.string.split())
         else:
             index = content.find("<body>")
             if index == -1:
                 return 0
             else:
                 titlePortion = content[:index]
-                return IndexWeights.getWordCount(self.searchPhrase,titlePortion.split())
+                return IndexWeights.getWordCount(self.searchToken,self.mweToken,titlePortion.split())
     def updateEntry(self,urlName,urlFile):
         try:    
-            baseFile = open(urlFile,"r")
-            html_content = baseFile.read()
+            baseFile = open(urlFile,"r",encoding="utf-8")
+            html_content = baseFile.read().lower()
             soup = BeautifulSoup(html_content,"lxml")
             titleCount = self.checkTitle(html_content,soup)
             if titleCount > 0:
                 self.queryDocument["priority"]["title"] += 1
+                self.queryDocument["tagSum"] += 1
             fileString = str(soup.find("body")).strip("<body>").strip("</body>")##get and return word count of base content first. Then check if it fits any tags
             #fileList = re.split(r'\W|\s',fileString)
             fileLines = re.split(r'\n',fileString)
@@ -109,10 +126,8 @@ class DbEntry:
             doc_lines = []
             currentLine = 1
             for line in fileLines:
-                noTags = re.split(TAG_EXP,line)
-                newString = "".join(noTags)
-                simpleList = re.split(r'\W+',newString)
-                base_count = IndexWeights.getWordCount(self.searchPhrase,simpleList)
+                simpleList = cleanUpTags(TAG_EXP,line)
+                base_count = IndexWeights.getWordCount(self.searchToken,self.mweToken,simpleList)
                 if base_count > 0:
                     doc_lines.append(currentLine)
                 currentLine += 1
@@ -123,7 +138,7 @@ class DbEntry:
                 tagList = soup.find_all(tag)
                 tagCount = 0
                 for item in tagList:
-                    if IndexWeights.getWordCount(self.searchPhrase,item.string.split()) > 0:
+                    if item.string != None and IndexWeights.getWordCount(self.searchToken,self.mweToken,item.string.split()) > 0:
                         tagCount += 1
                         totalTags += 1
                 self.queryDocument["priority"][tag] += tagCount
@@ -147,32 +162,42 @@ class DbEntry:
             baseFile.close()
         else:
             baseFile.close()
-def modifyObject(query,path):
-    pass
-def makeObject(query,path):
-    document = {}
-    return document
-def runQuery(query,fileName,dbCollection):
-    if dbCollection.find_one({}) == None:
-        makeObject(query,path,dbCollection)
-start = DbEntry(HOST,PORT,"HW3_Corpus","HW3_Corpus","Triangulation")
-print(start.queryDocument)
-start_pair = JSON_PAIRS[1]
-start.updateEntry("www.ics.uci.edu/~dock/manuals/cgal_manual/Triangulation_2_ref/Enum_Triangulation_2-Traits-Tds---Locate_type.html","WEBPAGES_CLEAN/34/311")
-print(start.queryDocument)
-#####    Sample Output on my Python3.5 Shell #########
-## ('12/187', 'www.ics.uci.edu/~wmt/ecoRaft') An example of file-url pairing from the bookkeeping.json file
-## 37497 Number of file-url pairings
-## {'tagRank': 0, 'count': 0, 'document_frequency': 0, '_id': ObjectId('5922611bcf54c717784070aa'), 'tagSum': 0, 'scores': [], 
-##  'file_matches': [], 'priority': {'h2': 0, 'h3': 0, 'title': 0, 'b': 0, 'h1': 0, 'strong': 0}, 'rank': -1, 'query': 'Triangulation'} 
-##  Object at initialization, search_query: 'Triangulation'
-## {'tagRank': 0, 'count': 4, 'document_frequency': 1, '_id': ObjectId('5922611bcf54c717784070aa'), 'tagSum': 0, 
-##  'scores': [{'WEBPAGES_CLEAN/34/311': 1.6020599913279623}], 'file_matches': [{'file_name': 'WEBPAGES_CLEAN/34/311', 
-##  'file_url': 'www.ics.uci.edu/~dock/manuals/cgal_manual/Triangulation_2_ref/Enum_Triangulation_2-Traits-Tds---Locate_type.html', 
-##  'lines': [37, 53, 54, 56]}], 'priority': {'h2': 0, 'h3': 0, 'title': 0, 'b': 0, 'h1': 0, 'strong': 0}, 'rank': -1, 'query': 'Triangulation'}
-##   Object after it had searched through a file that had its query in it.
+    def saveToDb(self):
+        self.mainCollection.save(self.queryDocument)
+def incrementalQuery(query,corpus,startIndex,stopIndex):
+    if startIndex < 0:
+        startIndex = 0
+    if stopIndex > 37497:
+        stopIndex = 37497
+    entry = DbEntry(HOST,PORT,"HW3_Corpus","HW3_Corpus",query)
+    prefix = "WEBPAGES_CLEAN/"
+    for row in range(startIndex,stopIndex):
+        entry.updateEntry(corpus[row][1],prefix + corpus[row][0])
+    return entry
+### A full run through of the WHOLE Corpus would essentially be 
+### incrementalQuery("search_term",TSV_PAIRS,0,DOC_TOTAL)
+###runQuery("Technical Interviews",TSV_PAIRS)    
 
-### can use start.mainCollection.save(start.queryDocument) to save and update entry to MongoDB database
+#print(start_pair)
+#start.updateEntry("www.ics.uci.edu/~dock/manuals/cgal_manual/Triangulation_2_ref/Enum_Triangulation_2-Traits-Tds---Locate_type.html","WEBPAGES_CLEAN/34/311")
+#start.mainCollection.save(start.queryDocument)
+##start2 = DbEntry(HOST,PORT,"HW3_Corpus","HW3_Corpus","Technical Interviews")
+##start2.updateEntry("wics.ics.uci.edu/author/leejacqueline?afg64_page_id=3&afg66_page_id=2&afg60_page_id=2&afg58_page_id=4&afg65_page_id=4","WEBPAGES_CLEAN/0/51")
+##print(start2.queryDocument)
+##start2.saveToDb()
 
-
-
+if __name__ == "__main__":
+    client = MongoClient(HOST,PORT)
+    use_db = client["HW3_Corpus"]
+    mainCollection = use_db["HW3_Corpus"]
+    docs = mainCollection.find({})
+    sample_obj = mainCollection.find_one({"query":"Mondingo"})
+    print(len(TSV_PAIRS))
+    ## The individual files do not contain any information 
+    print(TSV_PAIRS[0])
+    start = DbEntry(HOST,PORT,"HW3_Corpus","HW3_Corpus","Triangulation")
+    print(start.queryDocument)
+    print("\n\n")
+    irvineEntry = incrementalQuery("Irvine",TSV_PAIRS,30000,37497) ## Was testing and updating incrementally to check for bugs
+    print("DONE PROCESSING")
+    irvineEntry.saveToDb()
