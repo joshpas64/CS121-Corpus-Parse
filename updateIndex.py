ff954b8672b198815b3d00d625ab46257e507cf1
Joshua Pascascio <jpascasc@uci.edu>
2017-06-04 13:26:28 -0700

    Update updateIndex.py

--- updateIndex.py
+++ updateIndex.py
@@ -15,7 +15,7 @@ def runQuery(query):
     print("Done with run-through")
     indexToUpdate = queryIndex.preloadIndex() ## Update the index object and write it to the index files
     indexToUpdate = queryIndex.score() ## Implement the scoring technique on the object
-    
+    return indexToUpdate
 runQuery("ENTER YOUR QUERY HERE") ## You can put either one-word or multiword queries Corpus Parser handles multi-word queries and stopwords automatically     
         
     
